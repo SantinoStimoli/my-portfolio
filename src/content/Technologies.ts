63e4d50b54c82f751ede0806686dba7d70536991
Santino Stimoli <santinostimoli@gmail.com>
2024-02-22 16:22:01 -0300

    Hero with tags

---
+++ src/content/Technologies.ts
@@ -0,0 +1,30 @@
+export const Technologies: Technology[] = [
+  {
+    name: 'JavaScript',
+    icon: 'bxl-javascript',
+  },
+  {
+    name: 'TypeScript',
+    icon: 'bxl-typescript',
+  },
+  {
+    name: 'React',
+    icon: 'bxl-react',
+  },
+  {
+    name: 'Vue JS',
+    icon: 'bxl-vuejs',
+  },
+  {
+    name: 'Java',
+    icon: 'bxl-java',
+  },
+  {
+    name: 'Node JS',
+    icon: 'bxl-nodejs',
+  },
+  {
+    name: 'SQL',
+    icon: 'bx-data',
+  },
+]
