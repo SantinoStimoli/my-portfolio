1721b9633f98ab361e7cb4e9a5b0dc92c3dcfdda
Santino Stimoli <santinostimoli@gmail.com>
2024-02-23 10:37:31 -0300

    Study section

--- src/content/study.ts
+++ src/content/study.ts
@@ -1,1 +1,82 @@
-export const studyExperience: Experience[] = []
+export const studyExperience: Experience[] = [
+  {
+    title: 'Front End JAVA Developer',
+    company: 'Mindhub LA',
+    range: 'MAY 2022 - NOV 2022',
+    description: '',
+    technologies: [
+      {
+        name: 'HTML',
+        icon: 'bxl-html5',
+      },
+      {
+        name: 'CSS',
+        icon: 'bxl-css3',
+      },
+      {
+        name: 'Bootstrap',
+        icon: 'bxl-bootstrap',
+      },
+      {
+        name: 'JavaScript',
+        icon: 'bxl-javascript',
+      },
+      {
+        name: 'Vue',
+        icon: 'bxl-vuejs',
+      },
+      {
+        name: 'JAVA',
+        icon: 'bxl-java',
+      },
+      {
+        name: 'Spring',
+        icon: 'bxl-spring-boot',
+      },
+      {
+        name: 'SQL',
+        icon: 'bxl-data',
+      },
+      {
+        name: 'PostgreSQL',
+        icon: 'bxl-postgresql',
+      },
+    ],
+  },
+  {
+    title: 'MERN Developer',
+    company: 'OpenBootcamp',
+    range: 'NOV 2022 - MAY 2023',
+    description: '',
+    technologies: [
+      {
+        name: 'Tailwind',
+        icon: 'bxl-tailwind-css',
+      },
+      {
+        name: 'SASS',
+        icon: 'bxl-sass',
+      },
+      {
+        name: 'Vite',
+        icon: 'bxl-bootstrap',
+      },
+      {
+        name: 'TypeScript',
+        icon: 'bxl-typescript',
+      },
+      {
+        name: 'React',
+        icon: 'bxl-react',
+      },
+      {
+        name: 'Node',
+        icon: 'bxl-nodejs',
+      },
+      {
+        name: 'Express js',
+        icon: 'bxl-nodejs',
+      },
+    ],
+  },
+]
