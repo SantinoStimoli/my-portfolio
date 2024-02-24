bf86df4eece2f1fc5f53dd448bf990d295943f3d
Santino Stimoli <santinostimoli@gmail.com>
2024-02-24 18:31:38 -0300

    Projects content

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,8 +1,22 @@
+import SelatioLTD from '@assets/projects/SelatioLTD.png'
+import TUT88 from '@assets/projects/TUT88.png'
+import RNM from '@assets/projects/RNM.png'
+import Tecnicort from '@assets/projects/Presupuestos.png'
+import Alumnos from '@assets/projects/Alumnos-TKD.png'
+import CalculadoraGrupal from '@assets/projects/PlataGrupal.png'
+import InformacionIP from '@assets/projects/InformacionIP.png'
+import Calculadora from '@assets/projects/Calculadora.png'
+import DeteccionFacial from '@assets/projects/DeteccionFacial.png'
+import TaTeTi from '@assets/projects/TaTeTi.png'
+import PetShop from '@assets/projects/PetShop.png'
+import Rebel from '@assets/projects/RebelGames.png'
+import AmazingEvents from '@assets/projects/AmazingEvents.png'
+
 export const projects: Project[] = [
   {
     title: 'Selatio LTD (LP)',
     company: 'Net Duo SA',
-    img: '',
+    img: SelatioLTD.src,
     description: 'DESCRIPCION',
     pageLink: 'https://www.selatioltd.com/',
     technologies: ['bxl-react', 'bxl-javascript', 'bxl-tailwind-css'],
@@ -10,7 +24,7 @@ export const projects: Project[] = [
   {
     title: 'TUT88 (LP)',
     company: 'Net Duo SA',
-    img: '',
+    img: TUT88.src,
     description: 'DESCRIPCION',
     pageLink: 'https://tut88mkt.com/',
     technologies: ['bxl-wordpress'],
@@ -18,9 +32,99 @@ export const projects: Project[] = [
   {
     title: 'RNM (LP)',
     company: 'Net Duo SA',
-    img: '',
+    img: RNM.src,
     description: 'DESCRIPCION',
     pageLink: 'https://www.rnmglobalmkt.com/',
-    technologies: ['bxl-html5', 'bxl-css3', 'bxl-javascript'],
+    technologies: ['bxl-javascript', 'bxl-html5', 'bxl-css3'],
+  },
+  {
+    title: 'Generador de presupuestos',
+    company: 'Tecnicort',
+    img: Tecnicort.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/Tecnicort/',
+    codeLink: 'https://github.com/SantinoStimoli/Tecnicort',
+    technologies: ['bxl-react', 'bxl-typescript', 'bxl-tailwind-css'],
+  },
+  {
+    title: 'ERP alumnos',
+    company: 'UAT',
+    img: Alumnos.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/alumnos-tkd/',
+    codeLink: 'https://github.com/SantinoStimoli/alumnos-tkd',
+    technologies: ['bxl-firebase', 'bxl-react', 'bxl-typescript', 'bxl-tailwind-css'],
+  },
+  {
+    title: 'Calculador grupal',
+    company: 'Personal',
+    img: CalculadoraGrupal.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/plata-grupal/',
+    codeLink: 'https://github.com/SantinoStimoli/plata-grupal',
+    technologies: ['bxl-vuejs', 'bxl-typescript', 'bxl-tailwind-css'],
+  },
+  {
+    title: 'Información IP',
+    company: 'Personal',
+    img: InformacionIP.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/ip-information/',
+    codeLink: 'https://github.com/SantinoStimoli/ip-information',
+    technologies: ['bxl-javascript', 'bxl-html5', 'bxl-css3'],
+  },
+  {
+    title: 'Calculadora',
+    company: 'Personal',
+    img: Calculadora.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/calculator/',
+    codeLink: 'https://github.com/SantinoStimoli/calculator',
+    technologies: ['bxl-javascript', 'bxl-html5', 'bxl-css3'],
+  },
+  {
+    title: 'Detección facial',
+    company: 'Personal',
+    img: DeteccionFacial.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/validation-software/',
+    codeLink: 'https://github.com/SantinoStimoli/validation-software',
+    technologies: ['bxl-react', 'bxl-javascript', 'bxl-tailwind-css'],
+  },
+  {
+    title: 'Ta Te Ti',
+    company: 'Personal',
+    img: TaTeTi.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/Ta-Te-Ti/',
+    codeLink: 'https://github.com/SantinoStimoli/Ta-Te-Ti',
+    technologies: ['bxl-react', 'bxl-javascript', 'bxl-tailwind-css'],
+  },
+  {
+    title: 'PetShop',
+    company: 'Mindhub LA',
+    img: PetShop.src,
+    pageLink: 'https://santinostimoli.github.io/Franco-PetShop/',
+    codeLink: 'https://github.com/SantinoStimoli/Franco-PetShop',
+    description: 'DESCRIPCION',
+    technologies: ['bxl-vuejs', 'bxl-javascript', 'bxl-css3'],
+  },
+  {
+    title: 'Rebel Games',
+    company: 'Mindhub LA',
+    img: Rebel.src,
+    pageLink: 'https://santinostimoli.github.io/Rebel-Games/',
+    codeLink: 'https://github.com/SantinoStimoli/Rebel-Games',
+    description: 'DESCRIPCION',
+    technologies: ['bxl-vuejs', 'bxl-javascript', 'bxl-tailwind-css'],
+  },
+  {
+    title: 'Amazing Events',
+    company: 'Mindhub LA',
+    img: AmazingEvents.src,
+    description: 'DESCRIPCION',
+    pageLink: 'https://santinostimoli.github.io/Amazing-Events-React/',
+    codeLink: 'https://github.com/SantinoStimoli/Amazing-Events-React',
+    technologies: ['bxl-react', 'bxl-javascript', 'bxl-tailwind-css'],
   },
 ]
