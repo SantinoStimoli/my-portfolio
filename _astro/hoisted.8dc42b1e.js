ab2e777f6eab95fa80a2c588daf0ac12cdd16b0f
Santino Stimoli <santinostimoli@gmail.com>
2024-03-06 14:41:44 -0300

    Updates

--- _astro/hoisted.8dc42b1e.js
+++ _astro/hoisted.8dc42b1e.js
@@ -1,4 +1,4 @@
-import"https://unpkg.com/boxicons@2.1.4/dist/boxicons.js";const l=document.getElementById("start"),t=document.getElementById("bg-pic"),n=t?.clientHeight??0,o=t?.clientWidth??0;l?.addEventListener("mousemove",e=>{const{x:s,y:i}=e,a=(s-o/2)/o*-3,r=`
+import"./hoisted.ccb172af.js";import"https://unpkg.com/boxicons@2.1.4/dist/boxicons.js";const l=document.getElementById("start"),t=document.getElementById("bg-pic"),n=t?.clientHeight??0,o=t?.clientWidth??0;l?.addEventListener("mousemove",e=>{const{x:s,y:i}=e,a=(s-o/2)/o*-3,r=`
     perspective(500px)
     scale(1)
     rotateX(${(i-n/2)/n*-3}deg)
